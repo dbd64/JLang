ed7601e9997cd08134f007571937b9be1b88ff8e
Daniel Donenfeld <dbd64@users.noreply.github.com>
2019-04-13 22:21:02 -0700

    Try adding _JVM_GetClassTypeAnnotations to stubs

--- jdk/stubs.cpp
+++ jdk/stubs.cpp
@@ -27,4 +27,9 @@ void Polyglot_java_lang_Enum_compareTo__Ljava_lang_Object_2() {
     abort();
 }
 
+void _JVM_GetClassTypeAnnotations(){
+    fprintf(stderr, "This method should not be called\n");
+    abort();
+}
+
 } // extern "C"
